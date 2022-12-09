1211db3078e7be8722c24cb462984f189c04a169
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-09 10:25:37 +0530

    create proejct

---
+++ mapping-springboot/src/main/java/com/mapping/springboot/MappingSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.mapping.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappingSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MappingSpringbootApplication.class, args);
+	}
+
+}
