150386371e81a847f81d3358d24fad2657244bdf
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-09 18:27:12 +0530

    Create ApiResponce.java
    - It holds the architecture for API response.

---
+++ mapping-springboot/src/main/java/com/mapping/springboot/payload/ApiResponce.java
@@ -0,0 +1,20 @@
+package com.mapping.springboot.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ApiResponce {
+	
+	private String message;
+	private boolean isSuccess;
+	
+	
+}
