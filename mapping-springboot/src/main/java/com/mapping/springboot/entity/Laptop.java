6b59c240b496d2a463f6b266847a45fe306591d0
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-09 18:27:52 +0530

    Creating entities.
    - Creating two entities i.e. Student and Laptop.

---
+++ mapping-springboot/src/main/java/com/mapping/springboot/entity/Laptop.java
@@ -0,0 +1,5 @@
+package com.mapping.springboot.entity;
+
+public class Laptop {
+
+}
---
+++ mapping-springboot/src/main/java/com/mapping/springboot/entity/Student.java
@@ -0,0 +1,26 @@
+package com.mapping.springboot.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "jpa_student")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int studentId;
+	private String studentName;
+	private String about;
+}
