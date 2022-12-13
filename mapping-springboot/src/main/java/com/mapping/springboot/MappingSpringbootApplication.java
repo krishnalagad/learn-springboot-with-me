e708b931f897119d77bef0d3333f677aad9e7e62
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-13 21:30:46 +0530

    Update MappingSpringbootApplication.java

--- mapping-springboot/src/main/java/com/mapping/springboot/MappingSpringbootApplication.java
+++ mapping-springboot/src/main/java/com/mapping/springboot/MappingSpringbootApplication.java
@@ -11,6 +11,7 @@ import com.mapping.springboot.entity.Address;
 import com.mapping.springboot.entity.Category;
 import com.mapping.springboot.entity.Product;
 import com.mapping.springboot.entity.Student;
+import com.mapping.springboot.exceptions.ResourceNotFoundException;
 import com.mapping.springboot.repositories.CategoryRepository;
 import com.mapping.springboot.repositories.ProductRepository;
 import com.mapping.springboot.repositories.StudentRepository;
@@ -57,37 +58,41 @@ public class MappingSpringbootApplication implements CommandLineRunner {
 //		
 //		System.out.println(this.studentRepository.findById(102).get());
 
-		Product product1 = new Product();
-		product1.setPId("pro1");
-		product1.setProductName("Redmi Note 9");
+//		Product product1 = new Product();
+//		product1.setPId("pro1");
+//		product1.setProductName("Redmi Note 9");
+//
+//		Product product2 = new Product();
+//		product2.setPId("pro2");
+//		product2.setProductName("Realme Narzo");
+//
+//		Product product3 = new Product();
+//		product3.setPId("pro3");
+//		product3.setProductName("VU SmartTV");
+//
+//		Category category1 = new Category();
+//		category1.setCId("cat1");
+//		category1.setTitle("Electronics");
+//
+//		Category category2 = new Category();
+//		category2.setCId("cat2");
+//		category2.setTitle("Mobiles");
+//
+//		List<Product> category1Products = category1.getProducts();
+//		category1Products.add(product1);
+//		category1Products.add(product2);
+//		category1Products.add(product3);
+//
+//		List<Product> category2Products = category2.getProducts();
+//		category2Products.add(product1);
+//		category2Products.add(product2);
+//
+//		this.categoryRepository.save(category1);
+//		this.categoryRepository.save(category2);
 
-		Product product2 = new Product();
-		product2.setPId("pro2");
-		product2.setProductName("Realme Narzo");
-
-		Product product3 = new Product();
-		product3.setPId("pro3");
-		product3.setProductName("VU SmartTV");
-
-		Category category1 = new Category();
-		category1.setCId("cat1");
-		category1.setTitle("Electronics");
-
-		Category category2 = new Category();
-		category2.setCId("cat2");
-		category2.setTitle("Mobiles");
-
-		List<Product> category1Products = category1.getProducts();
-		category1Products.add(product1);
-		category1Products.add(product2);
-		category1Products.add(product3);
-
-		List<Product> category2Products = category2.getProducts();
-		category2Products.add(product1);
-		category2Products.add(product2);
-
-		this.categoryRepository.save(category1);
-		this.categoryRepository.save(category2);
+		Category category = this.categoryRepository.findById("cat2")
+				.orElseThrow(() -> new ResourceNotFoundException("Category", "ID", "cat1"));
+		System.out.println(category.getProducts().size());
 
 	}
 
