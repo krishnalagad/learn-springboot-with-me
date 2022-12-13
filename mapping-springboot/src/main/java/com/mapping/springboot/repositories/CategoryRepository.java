7b736eeb1d5326dce23a5490da2777bcaf9c8401
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-13 21:05:10 +0530

    Update CategoryRepository.java

--- mapping-springboot/src/main/java/com/mapping/springboot/repositories/CategoryRepository.java
+++ mapping-springboot/src/main/java/com/mapping/springboot/repositories/CategoryRepository.java
@@ -1,5 +1,9 @@
 package com.mapping.springboot.repositories;
 
-public interface CategoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mapping.springboot.entity.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, String> {
 
 }
