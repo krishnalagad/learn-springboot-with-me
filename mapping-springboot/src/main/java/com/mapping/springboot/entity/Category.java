deefe414b11cd4a6d3c03ce705b9128958addf97
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-13 21:30:36 +0530

    Update Category.java
    - FetchType

--- mapping-springboot/src/main/java/com/mapping/springboot/entity/Category.java
+++ mapping-springboot/src/main/java/com/mapping/springboot/entity/Category.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
@@ -25,7 +26,7 @@ public class Category {
 	private String cId;
 	private String title;
 	
-	@ManyToMany( cascade = CascadeType.ALL)
+	@ManyToMany( cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	List<Product> products = new ArrayList<Product>();
 
 }
