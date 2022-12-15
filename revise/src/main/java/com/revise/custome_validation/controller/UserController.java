af65bcf423d148686ac17d15e44d4414dd087ad7
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-15 23:03:13 +0530

    Update UserController.java
    - API to create user

--- revise/src/main/java/com/revise/custome_validation/controller/UserController.java
+++ revise/src/main/java/com/revise/custome_validation/controller/UserController.java
@@ -1,5 +1,27 @@
 package com.revise.custome_validation.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revise.custome_validation.entity.User;
+import com.revise.custome_validation.service.UserService;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping
+	public ResponseEntity<User> createUser(@RequestBody User user){
+		User createUser = this.userService.createUser(user);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createUser);
+	}
 
 }
