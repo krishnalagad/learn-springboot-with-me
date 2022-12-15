7bc02ed44dee2e61e449cae65b5526c38187d7e8
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-15 23:13:19 +0530

    Update UserController.java
    - Validation

--- revise/src/main/java/com/revise/custome_validation/controller/UserController.java
+++ revise/src/main/java/com/revise/custome_validation/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.revise.custome_validation.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +21,7 @@ public class UserController {
 	private UserService userService;
 	
 	@PostMapping
-	public ResponseEntity<User> createUser(@RequestBody User user){
+	public ResponseEntity<User> createUser(@Valid @RequestBody User user){
 		User createUser = this.userService.createUser(user);
 		return ResponseEntity.status(HttpStatus.CREATED).body(createUser);
 	}
