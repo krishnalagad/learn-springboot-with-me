9f93ee4b0a0c2ea69ef2e448990c7d89105fb6c2
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-15 23:50:07 +0530

    Update ImageNameValidator.java
    - Adding logger.

--- revise/src/main/java/com/revise/custome_validation/validate/ImageNameValidator.java
+++ revise/src/main/java/com/revise/custome_validation/validate/ImageNameValidator.java
@@ -3,12 +3,17 @@ package com.revise.custome_validation.validate;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ImageNameValidator implements ConstraintValidator<ImageNameValid, String> {
 	
-	
+	private Logger logger = LoggerFactory.getLogger(ImageNameValidator.class);
 
 	@Override
 	public boolean isValid(String value, ConstraintValidatorContext context) {
+		
+		logger.info("Message from isValid() : {}", value);
 
 		// logic
 		if (value.isBlank())
