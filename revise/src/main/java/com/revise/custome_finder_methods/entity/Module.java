9ddb642f02bd99b694c688d54b1951b56dca129c
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-27 19:11:21 +0530

    Defining entity fields.

--- revise/src/main/java/com/revise/custome_finder_methods/entity/Module.java
+++ revise/src/main/java/com/revise/custome_finder_methods/entity/Module.java
@@ -1,5 +1,30 @@
 package com.revise.custome_finder_methods.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "jpa_module")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Module {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int mId;
+	
+	private String moduleName;
+	private String moduleHead;
+	private int teamSize;
 
 }
