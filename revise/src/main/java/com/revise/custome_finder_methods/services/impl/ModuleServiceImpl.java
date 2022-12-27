ae679211657c07af0e130a60fb27829a99f1e2c6
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-27 19:17:41 +0530

    Implementing service methods

--- revise/src/main/java/com/revise/custome_finder_methods/services/impl/ModuleServiceImpl.java
+++ revise/src/main/java/com/revise/custome_finder_methods/services/impl/ModuleServiceImpl.java
@@ -1,5 +1,35 @@
 package com.revise.custome_finder_methods.services.impl;
 
-public class ModuleServiceImpl {
+import java.util.List;
+
+import com.revise.custome_finder_methods.entity.Module;
+import com.revise.custome_finder_methods.services.ModuleService;
+
+public class ModuleServiceImpl implements ModuleService {
+
+	@Override
+	public Module createModule(Module module) {
+		return null;
+	}
+
+	@Override
+	public Module updateModule(Module module) {
+		return null;
+	}
+
+	@Override
+	public Module getOneModule(Integer id) {
+		return null;
+	}
+
+	@Override
+	public List<Module> getAll() {
+		return null;
+	}
+
+	@Override
+	public void deleteModule(Integer id) {
+		
+	}
 
 }
