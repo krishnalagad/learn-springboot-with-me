650225b6b3ef8dfe67277de22d1c575f026b39ee
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-27 20:51:06 +0530

    custom finder method to get module whose head nane starts with

--- revise/src/main/java/com/revise/custome_finder_methods/repositories/ModuleRepository.java
+++ revise/src/main/java/com/revise/custome_finder_methods/repositories/ModuleRepository.java
@@ -7,6 +7,10 @@ import com.revise.custome_finder_methods.entity.Module;
 
 public interface ModuleRepository extends JpaRepository<Module, Integer> {
 	
+	// custom finder method to get modules by module head name.
 	List<Module> findByModuleHead(String name);
+	
+	// custom finder method to get module whose head nane starts with
+	List<Module> findByModuleNameStartingWith(String name);
 
 }
