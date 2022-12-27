b8a3a23ee6a56ce4bef7a2618b06f2f8c7fbdae8
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-27 20:28:22 +0530

    Adding custom finder method

--- revise/src/main/java/com/revise/custome_finder_methods/repositories/ModuleRepository.java
+++ revise/src/main/java/com/revise/custome_finder_methods/repositories/ModuleRepository.java
@@ -1,8 +1,12 @@
 package com.revise.custome_finder_methods.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.revise.custome_finder_methods.entity.Module;
 
 public interface ModuleRepository extends JpaRepository<Module, Integer> {
+	
+	List<Module> findByModuleHead(String name);
 
 }
