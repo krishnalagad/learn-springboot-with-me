7a02b03611ffb5a0f2527d8849dfc119eaca5eec
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-27 20:50:23 +0530

    get module starting with

--- revise/src/main/java/com/revise/custome_finder_methods/controllers/ModuleController.java
+++ revise/src/main/java/com/revise/custome_finder_methods/controllers/ModuleController.java
@@ -19,39 +19,45 @@ import com.revise.custome_finder_methods.services.ModuleService;
 @RestController
 @RequestMapping("/modules")
 public class ModuleController {
-	
+
 	@Autowired
 	private ModuleService moduleService;
-	
+
 	@PostMapping("/")
-	ResponseEntity<Module> saveModule(@RequestBody Module module){
+	ResponseEntity<Module> saveModule(@RequestBody Module module) {
 		Module createModule = this.moduleService.createModule(module);
 		return ResponseEntity.status(HttpStatus.CREATED).body(createModule);
 	}
-	
+
 	@PostMapping("/all")
-	ResponseEntity<List<Module>> saveAll(@RequestBody ArrayList<Module> modules){
+	ResponseEntity<List<Module>> saveAll(@RequestBody ArrayList<Module> modules) {
 		List<Module> createAllModules = this.moduleService.createAllModules(modules);
 		return ResponseEntity.ok(createAllModules);
 	}
-	
+
 	@GetMapping("/{id}")
-	ResponseEntity<Module> getModule(@PathVariable("id") Integer id){
+	ResponseEntity<Module> getModule(@PathVariable("id") Integer id) {
 		Module oneModule = this.moduleService.getOneModule(id);
 		return ResponseEntity.ok(oneModule);
 	}
-	
+
 	@GetMapping("/")
-	ResponseEntity<List<Module>> getAll(){
+	ResponseEntity<List<Module>> getAll() {
 		List<Module> all = this.moduleService.getAll();
 		return ResponseEntity.ok(all);
 	}
-	
+
 	@GetMapping("/head/{name}")
-	ResponseEntity<List<Module>> getModulesByName(@PathVariable("name") String name){
+	ResponseEntity<List<Module>> getModulesByName(@PathVariable("name") String name) {
 		System.out.println("controller: " + name);
 		List<Module> modules = this.moduleService.getModuleByHeadName(name.trim());
 		return ResponseEntity.ok(modules);
 	}
 
+	@GetMapping("/module-name/{prefix}")
+	ResponseEntity<List<Module>> getModulesStartWith(@PathVariable("prefix") String prefix) {
+		System.out.println("controller: " + prefix);
+		List<Module> modules = this.moduleService.getModuleStartsWith(prefix.trim());
+		return ResponseEntity.ok(modules);
+	}
 }
