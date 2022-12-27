941c18744358859030b1db1a28d01c03d02cb4f1
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-27 19:34:26 +0530

    Creating API methods in controller.

--- revise/src/main/java/com/revise/custome_finder_methods/controllers/ModuleController.java
+++ revise/src/main/java/com/revise/custome_finder_methods/controllers/ModuleController.java
@@ -1,5 +1,42 @@
 package com.revise.custome_finder_methods.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revise.custome_finder_methods.entity.Module;
+import com.revise.custome_finder_methods.services.ModuleService;
+
+@RestController
+@RequestMapping("/modules")
 public class ModuleController {
+	
+	@Autowired
+	private ModuleService moduleService;
+	
+	@PostMapping("/")
+	ResponseEntity<Module> saveModule(Module module){
+		Module createModule = this.moduleService.createModule(module);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createModule);
+	}
+	
+	@GetMapping("/{id}")
+	ResponseEntity<Module> getModule(@PathVariable("id") Integer id){
+		Module oneModule = this.moduleService.getOneModule(id);
+		return ResponseEntity.ok(oneModule);
+	}
+	
+	@GetMapping("/")
+	ResponseEntity<List<Module>> getAll(){
+		List<Module> all = this.moduleService.getAll();
+		return ResponseEntity.ok(all);
+	}
 
 }
