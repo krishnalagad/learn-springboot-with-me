211b820e34d226b884fd4689801f7b0c11893407
Krishna Lagad <krishnalagad2@gmail.com>
2022-12-31 14:57:07 +0530

    Update ModuleRepository.java

--- revise/src/main/java/com/revise/custome_finder_methods/repositories/ModuleRepository.java
+++ revise/src/main/java/com/revise/custome_finder_methods/repositories/ModuleRepository.java
@@ -6,20 +6,23 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.revise.custome_finder_methods.entity.Module;
 
 public interface ModuleRepository extends JpaRepository<Module, Integer> {
-	
+
 	// custom finder method to get modules by module head name.
 	List<Module> findByModuleHead(String name);
-	
+
 	// custom finder method to get module whose head nane starts with
 	List<Module> findByModuleNameStartingWith(String name);
-	
+
 	// custome finder method to search keyword in string
 	List<Module> findByModuleNameContaining(String name);
-	
+
 	// custome finder method to get results which not contains
 	List<Module> findByModuleNameNotContaining(String name);
-	
+
 	// custome finder method to get result starts with String
 	List<Module> findByModuleNameLike(String str);
+
+	// custome finder method to get result not starts with String
+	List<Module> findByModuleNameNotLike(String str);
 
 }
