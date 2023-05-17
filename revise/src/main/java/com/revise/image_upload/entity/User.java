34b55366b472e487fdfa676074500d787d158597
Krishna Lagad <krishnalagad2@gmail.com>
2023-05-17 11:43:25 +0530

    - create entity.

--- revise/src/main/java/com/revise/image_upload/entity/User.java
+++ revise/src/main/java/com/revise/image_upload/entity/User.java
@@ -1,4 +1,27 @@
 package com.revise.image_upload.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_table")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String imageName;
 }
