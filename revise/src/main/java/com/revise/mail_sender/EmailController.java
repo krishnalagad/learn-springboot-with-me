0859702b755d60693572671348e8fc9c506af1e4
Krishna Lagad <krishnalagad2@gmail.com>
2023-05-19 20:38:26 +0530

    - cross origin

--- revise/src/main/java/com/revise/mail_sender/EmailController.java
+++ revise/src/main/java/com/revise/mail_sender/EmailController.java
@@ -5,16 +5,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/email")
+@CrossOrigin("*")
 public class EmailController {
 
     @Autowired
