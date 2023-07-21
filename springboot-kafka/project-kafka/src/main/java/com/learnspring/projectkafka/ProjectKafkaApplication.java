c82a74715fdc9a2f52b8fab13ac5a915b529e04c
Krishna Lagad <krishnalagad2@gmail.com>
2023-07-21 19:50:07 +0530

    - dump root project.

---
+++ springboot-kafka/project-kafka/src/main/java/com/learnspring/projectkafka/ProjectKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.learnspring.projectkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectKafkaApplication.class, args);
+	}
+
+}
