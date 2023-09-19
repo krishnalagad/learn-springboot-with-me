e0488e566081834cda74a78952c0472795fd3b00
Krishna Lagad <krishnalagad2@gmail.com>
2023-09-19 13:42:59 +0530

    - dump project for springboot 3.2.0

---
+++ boot_3-2-0/src/main/java/com/learnspring/boot_320/Boot320Application.java
@@ -0,0 +1,13 @@
+package com.learnspring.boot_320;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot320Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot320Application.class, args);
+	}
+
+}
---
+++ boot_3-2-0/src/test/java/com/learnspring/boot_320/Boot320ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspring.boot_320;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot320ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
