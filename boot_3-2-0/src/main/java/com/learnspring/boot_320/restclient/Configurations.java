e278b4040417c71d07919113dafc90e822ff2d3e
Krishna Lagad <krishnalagad2@gmail.com>
2023-09-19 14:18:08 +0530

    -dump configurations of RestClient

---
+++ boot_3-2-0/src/main/java/com/learnspring/boot_320/restclient/Configurations.java
@@ -0,0 +1,18 @@
+package com.learnspring.boot_320.restclient;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class Configurations {
+
+    private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
+
+    @Bean
+    public RestClient restClient() {
+        return RestClient.builder()
+                .baseUrl(BASE_URL)
+                .build();
+    }
+}
