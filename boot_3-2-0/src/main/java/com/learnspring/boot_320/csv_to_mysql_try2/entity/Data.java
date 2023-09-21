d8cd5af9b290a809d809f17141543dd0761ac42c
Krishna Lagad <krishnalagad2@gmail.com>
2023-09-21 13:20:46 +0530

    - dump attempt 2

---
+++ boot_3-2-0/src/main/java/com/learnspring/boot_320/csv_to_mysql_try2/entity/Data.java
@@ -0,0 +1,24 @@
+package com.learnspring.boot_320.csv_to_mysql_try2.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "csv_to_mysql_try2_data")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Data {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private int end_year;
+    private int intensity;
+    private String sector;
+
+    @Column(length = 5000)
+    private String insight;
+
+}
