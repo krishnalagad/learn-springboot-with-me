b091230c8b30ed19e0a5214b4ab1ba26b3208644
Krishna Lagad <krishnalagad2@gmail.com>
2023-09-22 20:23:16 +0530

    - dump rest api to download file

--- boot_3-2-0/src/main/java/com/learnspring/boot_320/csv_to_mysql_apache/controller/FileController.java
+++ boot_3-2-0/src/main/java/com/learnspring/boot_320/csv_to_mysql_apache/controller/FileController.java
@@ -2,13 +2,15 @@ package com.learnspring.boot_320.csv_to_mysql_apache.controller;
 
 import com.learnspring.boot_320.csv_to_mysql_apache.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
 
 @RestController
 @RequestMapping("/api/v1/csv")
@@ -26,5 +28,15 @@ public class FileController {
         }
 
         return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("Please upload CSV file only.");
+    }
+
+    @GetMapping("/download")
+    public ResponseEntity<?> downloadFile() {
+        String fileName = "download_data.csv";
+        ByteArrayInputStream fileData = this.fileService.load();
+        InputStreamResource resource = new InputStreamResource(fileData);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName)
+                .contentType(MediaType.parseMediaType("application/csv"))
+                .body(resource);
     }
 }
