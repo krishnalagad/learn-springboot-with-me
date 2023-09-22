e15b9bfc1b20a0913c78c0c3066db0dc093589a7
Krishna Lagad <krishnalagad2@gmail.com>
2023-09-22 17:55:03 +0530

    - comment out code

---
+++ boot_3-2-0/src/main/java/com/learnspring/boot_320/csv_to_mysql_springbatch/entity/Data.java
@@ -0,0 +1,52 @@
+//package com.learnspring.boot_320.csv_to_mysql.entity;
+//
+////import jakarta.persistence.*;
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.NoArgsConstructor;
+//import lombok.ToString;
+//
+//@Entity
+//@Table(name = "csv_to_mysql_data")
+//@lombok.Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@ToString
+//public class Data {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private int id;
+//
+//    private int end_year;
+////    private double citylng;
+////    private double citylat;
+//    private int intensity;
+//    private String sector;
+////    private String topic;
+//
+//    @Column(length = 5000)
+//    private String insight;
+//
+////    private String swot;
+////
+////    @Column(length = 1000)
+////    private String url;
+////    private String region;
+////    private int start_year;
+////    private int impact;
+////    private String added;
+////    private String published;
+////    private String city;
+////    private String country;
+////    private int relevance;
+////    private String pestle;
+////
+////    @Column(length = 500)
+////    private String source;
+////
+////    @Column(length = 1000)
+////    private String title;
+////    private int likelihood;
+//
+//}
