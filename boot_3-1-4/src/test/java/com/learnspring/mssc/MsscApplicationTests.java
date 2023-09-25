49c66739b96851d546e538c3bc337c0834ca5f97
Krishna Lagad <krishnalagad2@gmail.com>
2023-09-25 15:47:27 +0530

    - create new spring boot project for multiple spring security configurations

---
+++ boot_3-1-4/src/test/java/com/learnspring/mssc/MsscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspring.mssc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
