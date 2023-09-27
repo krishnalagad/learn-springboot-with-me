951ed8f581c77e2f0c5e775101d8ff55d396a8f3
Krishna Lagad <krishnalagad2@gmail.com>
2023-09-27 22:48:47 +0530

    - convert post record into class

--- boot_3-2-0/src/main/java/com/learnspring/boot_320/jsonfile_to_mysql/entity/Post.java
+++ boot_3-2-0/src/main/java/com/learnspring/boot_320/jsonfile_to_mysql/entity/Post.java
@@ -3,10 +3,23 @@ package com.learnspring.boot_320.jsonfile_to_mysql.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "json_to_mysql_post")
-public record Post(@Id String id, String title, String slug, LocalDate date, int timeToRead, String tags) {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+    @Id
+    String id;
+    String title;
+    String slug;
+    LocalDate date;
+    int timeToRead;
+    String tags;
 }
