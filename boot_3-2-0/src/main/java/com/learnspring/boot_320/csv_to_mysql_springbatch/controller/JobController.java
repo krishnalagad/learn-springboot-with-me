a9fae51d9f67b695601b4d159984b87fa61fbf62
Krishna Lagad <krishnalagad2@gmail.com>
2023-10-10 20:44:02 +0530

    - Working fine.

--- boot_3-2-0/src/main/java/com/learnspring/boot_320/csv_to_mysql_springbatch/controller/JobController.java
+++ boot_3-2-0/src/main/java/com/learnspring/boot_320/csv_to_mysql_springbatch/controller/JobController.java
@@ -1,10 +1,7 @@
 package com.learnspring.boot_320.csv_to_mysql_springbatch.controller;
 
 import com.learnspring.boot_320.csv_to_mysql_springbatch.service.FileService;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
@@ -16,6 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/api/v1/jobs")
@@ -29,6 +30,8 @@ public class JobController {
 
     @Autowired
     private FileService fileService;
+
+    private final String PATH = "D:\\Installed Software\\Github Desktop\\learn-springboot-with-me\\boot_3-2-0\\src\\main\\resources\\";
 
     @PostMapping("/import")
     public ResponseEntity<?> csvToDb() {
@@ -49,16 +52,25 @@ public class JobController {
     @PostMapping("/upload")
     public ResponseEntity<?> uploadAndSaveFile(@RequestParam("file") MultipartFile file) throws Exception {
 
-        String fileName = this.fileService.saveFileToDirectory(file);
+//        String fileName = this.fileService.saveFileToDirectory(file);
+        String fileName = file.getOriginalFilename();
+        File fileToImport = new File(this.PATH + fileName);
+        Files.deleteIfExists(Paths.get(this.PATH + fileName));
 
-//        JobParameters jobParameters = new JobParametersBuilder()
-//                .addLong("startAt", System.currentTimeMillis()).toJobParameters();
-//        try {
-//            this.jobLauncher.run(this.job, jobParameters);
-//        } catch (JobExecutionAlreadyRunningException | JobParametersInvalidException |
-//                 JobInstanceAlreadyCompleteException | JobRestartException e) {
-//            throw new RuntimeException(e);
-//        }
+        file.transferTo(fileToImport);
+
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("startAt", System.currentTimeMillis()).toJobParameters();
+        try {
+            JobExecution execution = this.jobLauncher.run(this.job, jobParameters);
+            if (execution.getExitStatus().getExitCode().equals(ExitStatus.COMPLETED)) {
+                // delete file after completing operation.
+                Files.deleteIfExists(Paths.get(this.PATH + fileName));
+            }
+        } catch (JobExecutionAlreadyRunningException | JobParametersInvalidException |
+                 JobInstanceAlreadyCompleteException | JobRestartException e) {
+            throw new RuntimeException(e);
+        }
         return ResponseEntity.ok(fileName);
     }
 }
