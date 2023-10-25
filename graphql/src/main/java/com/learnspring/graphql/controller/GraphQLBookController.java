f5adccc5569a1e8dcf721cd9792b05ee66343159
Krishna Lagad <krishnalagad2@gmail.com>
2023-10-25 19:35:37 +0530

    - fix required

--- graphql/src/main/java/com/learnspring/graphql/controller/GraphQLBookController.java
+++ graphql/src/main/java/com/learnspring/graphql/controller/GraphQLBookController.java
@@ -2,10 +2,14 @@ package com.learnspring.graphql.controller;
 
 import com.learnspring.graphql.entity.Book;
 import com.learnspring.graphql.service.BookService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -16,6 +20,13 @@ public class GraphQLBookController {
     @Autowired
     private BookService bookService;
 
+    @MutationMapping("createBook")
+    public Book create(@Argument BookInput bookInput) {
+//        Book book = new Book(0, bookInput.getTitle(), bookInput.getDesc(), bookInput.getAuthor(),
+//                bookInput.getPrice(), bookInput.getPages());
+        return this.bookService.createBook(book);
+    }
+
     @QueryMapping("allBooks")
     public List<Book> getAll() {
         return this.bookService.getBooks();
@@ -25,4 +36,16 @@ public class GraphQLBookController {
     public Book getBook(@Argument Integer bookId) {
         return this.bookService.getBook(bookId);
     }
+}
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+class BookInput {
+    private String title;
+    private String desc;
+    private String author;
+    private double price;
+    private int pages;
 }
