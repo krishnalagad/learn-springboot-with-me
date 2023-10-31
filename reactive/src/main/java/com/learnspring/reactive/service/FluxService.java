27df5d0b865eeebc07501ea7f87651e12c440270
Krishna Lagad <krishnalagad2@gmail.com>
2023-10-31 16:59:29 +0530

    - dump Flux

---
+++ reactive/src/main/java/com/learnspring/reactive/service/FluxService.java
@@ -0,0 +1,21 @@
+package com.learnspring.reactive.service;
+
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+import java.util.*;
+
+@Service
+public class FluxService {
+
+    // return publisher -> Flux
+    public Flux<String> getFlux() {
+        return Flux.just("DEFAULT", "FLUX").log();
+    }
+
+    // return publisher -> Flux from any collection
+    public Flux<String> getFluxFromCollection() {
+        List<String> fruits = List.of("Mango", "Cherry", "Banana", "Apple", "Cranberry", "Grape", "Jackfruit");
+        return Flux.fromIterable(fruits).log();
+    }
+}
---
+++ reactive/src/test/java/com/learnspring/reactive/FluxTest.java
@@ -0,0 +1,29 @@
+package com.learnspring.reactive;
+
+import com.learnspring.reactive.service.FluxService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FluxTest {
+
+    @Autowired
+    private FluxService fluxService;
+
+    @Test
+    void fluxTest1() {
+        this.fluxService.getFlux().subscribe(data -> {
+            System.out.println(data);
+            System.out.println("Done with flux data.");
+        });
+    }
+
+    @Test
+    void fluxTest2() {
+        this.fluxService.getFluxFromCollection().subscribe(data -> {
+            System.out.println(data);
+            System.out.println("Done with flux data.");
+        });
+    }
+}
