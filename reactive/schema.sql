e378ba3d1d7b0eaa73e783c772bd4c0d0e9ee238
Krishna Lagad <krishnalagad2@gmail.com>
2023-11-01 23:28:40 +0530

    - dump sql file to store sql query for table creation.

---
+++ reactive/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE webflux_books (
+	book_id INT PRIMARY KEY AUTO_INCREMENT,
+    book_name VARCHAR(255),
+    book_desc TEXT,
+    book_publisher VARCHAR(255),
+    book_author VARCHAR(255)
+)
