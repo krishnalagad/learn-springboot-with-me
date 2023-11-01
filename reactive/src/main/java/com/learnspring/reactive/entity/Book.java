44058760b6e042fab0bb0d65496a0802b638c24d
Krishna Lagad <krishnalagad2@gmail.com>
2023-11-01 23:19:18 +0530

    - dump getters, setters, constructors, tostring()

--- reactive/src/main/java/com/learnspring/reactive/entity/Book.java
+++ reactive/src/main/java/com/learnspring/reactive/entity/Book.java
@@ -1,15 +1,32 @@
 package com.learnspring.reactive.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("webflux_books")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Book {
 
     @Id
     private Integer bookId;
+
+    @Column("book_name")
     private String name;
+
+    @Column("book_desc")
     private String description;
+
+    @Column("book_publisher")
     private String publisher;
+
+    @Column("book_author")
     private String author;
 }
