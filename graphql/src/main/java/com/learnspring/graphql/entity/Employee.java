7bafa1ae55226ed0eb9990cffdc91d3dbe89b9e3
Krishna Lagad <krishnalagad2@gmail.com>
2023-11-06 13:25:20 +0530

    Update Employee.java

--- graphql/src/main/java/com/learnspring/graphql/entity/Employee.java
+++ graphql/src/main/java/com/learnspring/graphql/entity/Employee.java
@@ -1,4 +1,28 @@
 package com.learnspring.graphql.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "aulacube_emp")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Department department;
 }
