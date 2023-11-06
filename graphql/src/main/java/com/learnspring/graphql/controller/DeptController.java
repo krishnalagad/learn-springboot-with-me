6cc53ce04c3cc08e52e375d4fa4c1ad631c673f6
Krishna Lagad <krishnalagad2@gmail.com>
2023-11-06 13:56:33 +0530

    - dump upadate

--- graphql/src/main/java/com/learnspring/graphql/controller/DeptController.java
+++ graphql/src/main/java/com/learnspring/graphql/controller/DeptController.java
@@ -1,4 +1,24 @@
 package com.learnspring.graphql.controller;
 
+import com.learnspring.graphql.entity.Department;
+import com.learnspring.graphql.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/dept")
 public class DeptController {
+
+    @Autowired
+    private DeptService deptService;
+
+    @PostMapping("/")
+    public ResponseEntity<?> create(@RequestBody Department department) {
+        Department dept = this.deptService.create(department);
+        return ResponseEntity.status(201).body(dept);
+    }
 }
