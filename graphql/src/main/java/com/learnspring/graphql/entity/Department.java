a21fd31c7544fd5e1e9679a631f2882aa1203545
Krishna Lagad <krishnalagad2@gmail.com>
2023-11-06 13:25:13 +0530

    Update Department.java

--- graphql/src/main/java/com/learnspring/graphql/entity/Department.java
+++ graphql/src/main/java/com/learnspring/graphql/entity/Department.java
@@ -1,4 +1,28 @@
 package com.learnspring.graphql.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "aulacube_dept")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Department {
+
+    @Id
+    private String departmentId;
+    private String departmentName;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    @OneToMany(mappedBy = "department", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Employee> employees = new ArrayList<>();
+
 }
