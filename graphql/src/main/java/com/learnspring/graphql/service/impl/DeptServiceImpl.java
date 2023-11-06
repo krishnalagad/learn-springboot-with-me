47d7d01224337ab215a1bfcd800da0ee1be06807
Krishna Lagad <krishnalagad2@gmail.com>
2023-11-06 13:39:02 +0530

    - dump business logic of dept service

--- graphql/src/main/java/com/learnspring/graphql/service/impl/DeptServiceImpl.java
+++ graphql/src/main/java/com/learnspring/graphql/service/impl/DeptServiceImpl.java
@@ -1,4 +1,54 @@
 package com.learnspring.graphql.service.impl;
 
-public class DeptServiceImpl {
+import com.learnspring.graphql.entity.Department;
+import com.learnspring.graphql.repository.DeptRepository;
+import com.learnspring.graphql.repository.EmpRepository;
+import com.learnspring.graphql.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DeptServiceImpl implements DeptService {
+
+    @Autowired
+    private DeptRepository deptRepository;
+
+    @Autowired
+    private EmpRepository empRepository;
+
+    @Override
+    public Department create(Department department) {
+        return this.deptRepository.save(department);
+    }
+
+    @Override
+    public Department update(Department department, String id) {
+        Department dept = this.deptRepository.findById(id).orElseThrow(() -> new RuntimeException(
+                String.format("Department of Id: %s is not found", id)));
+        dept.setDepartmentName(department.getDepartmentName());
+        return this.deptRepository.save(dept);
+    }
+
+    @Override
+    public Department getDept(String id) {
+        return this.deptRepository.findById(id).orElseThrow(() -> new RuntimeException(
+                String.format("Department of Id: %s is not found", id)));
+    }
+
+    @Override
+    public List<Department> getAll() {
+        return this.deptRepository.findAll();
+    }
+
+    @Override
+    public void delete(String id) {
+        Department dept = this.deptRepository.findById(id).orElseThrow(() -> new RuntimeException(
+                String.format("Department of Id: %s is not found", id)));
+        if (!dept.getEmployees().isEmpty()) {
+            this.empRepository.deleteAll(dept.getEmployees());
+        }
+        this.deptRepository.delete(dept);
+    }
 }
