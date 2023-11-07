f327a3a5767f926678bf29f45e022fa08f7c0fc6
Krishna Lagad <krishnalagad2@gmail.com>
2023-11-07 12:14:40 +0530

    - dump custom finder method

--- graphql/src/main/java/com/learnspring/graphql/repository/EmpRepository.java
+++ graphql/src/main/java/com/learnspring/graphql/repository/EmpRepository.java
@@ -1,7 +1,11 @@
 package com.learnspring.graphql.repository;
 
+import com.learnspring.graphql.entity.Department;
 import com.learnspring.graphql.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface EmpRepository extends JpaRepository<Employee, Integer> {
+    Set<Employee> findByDepartment(Department department);
 }
