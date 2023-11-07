d7f383961324256d94918e6ca9403daeb5f0c23a
Krishna Lagad <krishnalagad2@gmail.com>
2023-11-07 14:48:13 +0530

    - dump custom finder method.

--- reactive/src/main/java/com/learnspring/reactive/repository/BookRepository.java
+++ reactive/src/main/java/com/learnspring/reactive/repository/BookRepository.java
@@ -1,9 +1,14 @@
 package com.learnspring.reactive.repository;
 
 import com.learnspring.reactive.entity.Book;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 @Repository
 public interface BookRepository extends ReactiveCrudRepository<Book, Integer> {
+
+    @Query("select * from webflux_books where book_name LIKE :title")
+    Flux<Book> searchBookByTitle(String title);
 }
