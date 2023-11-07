0309c126f7161ec3c684d6f50124d13ef0ab0610
Krishna Lagad <krishnalagad2@gmail.com>
2023-11-07 12:21:32 +0530

    - dump api

--- graphql/src/main/java/com/learnspring/graphql/controller/EmpController.java
+++ graphql/src/main/java/com/learnspring/graphql/controller/EmpController.java
@@ -4,12 +4,12 @@ import com.learnspring.graphql.entity.Employee;
 import com.learnspring.graphql.service.EmpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/v1/emp")
@@ -37,7 +37,7 @@ public class EmpController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Employee> upadte(@RequestBody Employee employee, @PathVariable Integer id) {
+    public ResponseEntity<Employee> update(@RequestBody Employee employee, @PathVariable Integer id) {
         Employee update = this.empService.update(employee, id);
         return ResponseEntity.status(HttpStatus.OK).body(update);
     }
@@ -46,5 +46,11 @@ public class EmpController {
     public ResponseEntity<?> delete(@PathVariable Integer id) {
         this.empService.delete(id);
         return ResponseEntity.status(204).body(Map.of("response", "Employee deleted successfully"));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findByDept(@PathVariable String id) {
+        Set<Employee> emps = this.empService.findEmployeesByDepartment(id);
+        return ResponseEntity.ok(emps);
     }
 }
