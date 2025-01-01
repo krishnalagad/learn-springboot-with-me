731eacc57d08ca0c7b76120946015c07132cacd9
Krishna Lagad <krishnalagad2@gmail.com>
2025-01-01 21:33:20 +0530

    added customized exception response entity handler for the project

---
+++ microservices/restful-web-apis/src/main/java/com/learnspring/webservices/restful_web_apis/exceptions/CustomizedExceptionHandlerResponseEntityHandler.java
@@ -0,0 +1,31 @@
+package com.learnspring.webservices.restful_web_apis.exceptions;
+
+import com.learnspring.webservices.restful_web_apis.utils.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class CustomizedExceptionHandlerResponseEntityHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+}
--- microservices/restful-web-apis/src/main/java/com/learnspring/webservices/restful_web_apis/exceptions/UserNotFoundException.java
+++ microservices/restful-web-apis/src/main/java/com/learnspring/webservices/restful_web_apis/exceptions/UserNotFoundException.java
@@ -1,5 +1,9 @@
 package com.learnspring.webservices.restful_web_apis.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String s) {
         super(s);
