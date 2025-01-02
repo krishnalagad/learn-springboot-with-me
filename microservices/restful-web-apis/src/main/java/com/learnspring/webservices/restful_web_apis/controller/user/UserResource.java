41cb1d71d57cb9bf220876ed8f4cbafd68ea0ae1
Krishna Lagad <krishnalagad2@gmail.com>
2025-01-02 19:49:58 +0530

    added dependency for documenting rest apis in the project

--- microservices/restful-web-apis/src/main/java/com/learnspring/webservices/restful_web_apis/controller/user/UserResource.java
+++ microservices/restful-web-apis/src/main/java/com/learnspring/webservices/restful_web_apis/controller/user/UserResource.java
@@ -3,6 +3,7 @@ package com.learnspring.webservices.restful_web_apis.controller.user;
 import com.learnspring.webservices.restful_web_apis.entity.user.User;
 import com.learnspring.webservices.restful_web_apis.exceptions.UserNotFoundException;
 import com.learnspring.webservices.restful_web_apis.service.user.UserDaoService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -38,13 +39,18 @@ public class UserResource {
     }
 
     @PostMapping("/users")
-    public ResponseEntity<?> saveUser(@RequestBody User user) {
+    public ResponseEntity<?> saveUser(@Valid @RequestBody User user) {
         User savedUser = this.userDaoService.save(user);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
                 .buildAndExpand(savedUser.getId())
                 .toUri();
         return ResponseEntity.created(location).body("User created successfully.");
+    }
+
+    @GetMapping("/users/i18n-api")
+    public String helloWorldI18N() {
+        return "Hello world, welcome to the world of Springboot!!";
     }
 
 }
