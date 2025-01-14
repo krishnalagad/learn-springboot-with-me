c4e54a287c3a6d16ab90a20507271b9146b8b255
Krishna Lagad <krishnalagad2@gmail.com>
2025-01-14 20:46:05 +0530

    add configuration file

---
+++ microservices/limit-service/src/main/java/com/learnspring/springcloud/limit_service/Application.java
@@ -0,0 +1,22 @@
+package com.learnspring.springcloud.limit_service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	private final static Logger logger = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		logger.info("Application started.");
+	}
+}
---
+++ microservices/limit-service/src/main/java/com/learnspring/springcloud/limit_service/controller/LimitsController.java
@@ -0,0 +1,19 @@
+package com.learnspring.springcloud.limit_service.controller;
+
+import com.learnspring.springcloud.limit_service.beans.Limits;
+import com.learnspring.springcloud.limit_service.config.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+    }
+}
