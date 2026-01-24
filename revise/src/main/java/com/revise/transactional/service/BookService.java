8665f1402d0104f8d3344d1f1f6827cb42f73a47
Krishna Lagad <krishnalagad2@gmail.com>
2026-01-24 19:36:04 +0530

    adding bean lifecycle

--- revise/src/main/java/com/revise/transactional/service/BookService.java
+++ revise/src/main/java/com/revise/transactional/service/BookService.java
@@ -2,14 +2,17 @@ package com.revise.transactional.service;
 
 import com.revise.transactional.entity.Book;
 import com.revise.transactional.repo.BookRepository;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Service
-public class BookService {
+public class BookService implements InitializingBean, DisposableBean {
     @Autowired
     private BookRepository bookRepository;
 
@@ -23,7 +26,7 @@ public class BookService {
     public Book createBook(Book book) { return bookRepository.save(book); }
 
     @Transactional
-    public Book updateBook(Long id, Book details) {
+    public Book updateBook(Long id, @org.jetbrains.annotations.NotNull @NotNull Book details) {
         Book book = getBookById(id);
         book.setTitle(details.getTitle());
         book.setAuthor(details.getAuthor());
@@ -33,4 +36,14 @@ public class BookService {
 
     @Transactional
     public void deleteBook(Long id) { bookRepository.deleteById(id); }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Bean created.");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Bean is going to destroy.");
+    }
 }
