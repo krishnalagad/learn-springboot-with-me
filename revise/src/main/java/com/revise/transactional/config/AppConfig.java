8b240a57726031bdd87d406f6be4f1ac3b7368cc
Krishna Lagad <krishnalagad2@gmail.com>
2026-01-25 19:15:21 +0530

    custom filter

--- revise/src/main/java/com/revise/transactional/config/AppConfig.java
+++ revise/src/main/java/com/revise/transactional/config/AppConfig.java
@@ -1,9 +1,25 @@
 package com.revise.transactional.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
 @Configuration
 @EnableCaching
-public class AppConfig {
+public class AppConfig implements Filter {
+
+    private static final Logger logger = LoggerFactory.getLogger(AppConfig.class);
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        logger.info("Request URL: {}", request.getRequestURL().toString());
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
 }
