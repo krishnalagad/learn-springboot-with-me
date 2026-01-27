d68c77e0733d1cd670516c7b9888a4d0e30b877e
Krishna Lagad <krishnalagad2@gmail.com>
2026-01-27 22:16:26 +0530

    jwt token generation

--- revise/src/main/java/com/revise/transactional/controller/AuthController.java
+++ revise/src/main/java/com/revise/transactional/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.revise.transactional.controller;
 
 import com.revise.transactional.entity.AuthRequest;
+import com.revise.transactional.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,15 +15,17 @@ public class AuthController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
     @PostMapping("/auth")
     public String generateToken(@RequestBody AuthRequest authRequest) {
         try {
             this.authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
             );
-            // TODO: Generate JWT Token
 
-            return "JWT TOKEN";
+            return this.jwtUtil.generateToken(authRequest.getUsername());
         } catch (Exception e) {
             throw e;
         }
--- revise/src/main/java/com/revise/transactional/util/JwtUtil.java
+++ revise/src/main/java/com/revise/transactional/util/JwtUtil.java
@@ -1,7 +1,24 @@
 package com.revise.transactional.util;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
 
 @Component
 public class JwtUtil {
+    private static final long EXPIRATION_TIME = 1000*60*60;
+    private final String SECRET = "too-long-secret-key-to-generate-jwt-token-1234567890@34";
+    private final SecretKey key = Keys.hmacShaKeyFor(SECRET.getBytes());
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
 }
