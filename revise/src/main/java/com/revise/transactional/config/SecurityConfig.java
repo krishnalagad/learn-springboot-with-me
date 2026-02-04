38ba291b7d3ac73a96e6904f0d1cc06bfe9fcbca
Krishna Lagad <krishnalagad2@gmail.com>
2026-02-04 23:23:57 +0530

    jwt authentication

--- revise/src/main/java/com/revise/transactional/config/SecurityConfig.java
+++ revise/src/main/java/com/revise/transactional/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.revise.transactional.config;
 
+import com.revise.transactional.filter.JwtAuthFilter;
 import com.revise.transactional.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,11 +15,15 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -27,8 +33,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 new AntPathRequestMatcher("/api/books/all"),
                                 new AntPathRequestMatcher("/auth")).permitAll() // Specific first
-                        .anyRequest().authenticated())                 // General last
-                .httpBasic(Customizer.withDefaults());
+                        .anyRequest().authenticated());                // General last;
+        httpSecurity.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
--- revise/src/main/java/com/revise/transactional/util/JwtUtil.java
+++ revise/src/main/java/com/revise/transactional/util/JwtUtil.java
@@ -1,8 +1,10 @@
 package com.revise.transactional.util;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -20,5 +22,28 @@ public class JwtUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean validateToken(String username, UserDetails userDetails, String token) {
+        // TODO -> check if the username is same as username in userDetails
+        // TODO -> check if token is not expired
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaims(token).getExpiration().before(new Date());
     }
 }
