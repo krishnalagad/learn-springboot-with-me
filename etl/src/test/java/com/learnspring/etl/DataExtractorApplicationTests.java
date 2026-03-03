e75843c9c05ebebeeed102e6f5016110c8a830d3
Krishna Lagad <krishnalagad2@gmail.com>
2026-03-03 18:13:59 +0530

    ETL

---
+++ etl/src/test/java/com/learnspring/etl/DataExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspring.etl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
